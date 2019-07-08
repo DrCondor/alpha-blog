424ec3f6485ded83c93c8c5b0f9ab6029659992e
Konrad Łuczaj <konradluczaj1@gmail.com>
2019-07-08 21:37:23 +0200

    new article form

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Article < ApplicationRecord
 validates :title, presence: true, length: { minimum: 3, maximum: 50 }
 validates :description, presence: true, length: { minimum: 15, maximum: 300 }
