4110a8ef3ef17718bc69446a3dd921c1a18d1893
Konrad Łuczaj <konradluczaj1@gmail.com>
2019-07-09 22:42:43 +0200

    show action, flash and error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,20 @@
 class ArticlesController < ApplicationController
-  def index
-
-  end
-
   def new
     @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to article_path(@article)
+    if @article.save
+      flash[:notice] = 'Article was successfully created'
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
 
   def show
-    @articles = Article.all
+    @article = Article.find(params[:id])
   end
 
   private
